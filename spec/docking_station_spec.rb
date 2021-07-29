6a42337c418ce447265e74a87b38c9646d9f9b17
Mubashar Ali <mubashar.jaffri@gmail.com>
2021-07-29 14:16:58 +0000

    chapter10 required bike file in docking_station and defined working? and bike.new in docking_station

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,13 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+  
+  it 'gets a working bike' do
+   bike = subject.release_bike
+   expect(bike).to be_working
+  end  
 end
+
+
